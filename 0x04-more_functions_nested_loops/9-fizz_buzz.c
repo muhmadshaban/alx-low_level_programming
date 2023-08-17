42a8f3352d43496abdf789648041a7a597c66951
muhmadshaban <moshaban96.eng@gmail.com>
2023-08-17 14:52:56 +0300

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ ** main - entery point
+ **
+ ** Return: Always 0
+ **/
+int main(void)
+
+{
+	int i = 1;
+
+
+	while (i <= 100)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+
+	printf("FizzBuzz");
+
+	else if (i % 3 == 0)
+
+	printf("Fizz");
+
+	else if (i % 5 == 0)
+
+	printf("Buzz");
+
+	else
+
+	printf("%d", i);
+	if (i != 100)
+	putchar(' ');
+	i++;
+}
+
+	printf("\n");
+
+
+	return (0);
+}
