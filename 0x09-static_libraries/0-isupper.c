51d1a93381202cbac66ab598490bd4f67a44220e
muhmadshaban <moshaban96.eng@gmail.com>
2023-09-03 00:15:43 +0300

    libmy

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,22 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- ** _isupper - If is uppercase return 1, if not return 0
- **
- ** @c: this is the entry
- **
- ** Return: 0
- **
+ ** _isupper - checks for uppercase character
+ ** @c: character to check
+ ** Return: 1 if c is uppercase, 0 otherwise
  **/
 int _isupper(int c)
 
 {
+
 	if (c >= 'A' && c <= 'Z')
-{
 	return (1);
-}
-	else
-{
 	return (0);
-}
 }
