bc4275df7f41861fe60279f33c747b01f6f4577f
muhmadshaban <moshaban96.eng@gmail.com>
2023-09-21 20:38:59 +0300

    1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ ** firstRun - check the code
+ **
+ ** Return: Always 0.
+ **/
+void firstRun(void) __attribute__ ((constructor));
+void firstRun(void)
+
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+
+
+}
