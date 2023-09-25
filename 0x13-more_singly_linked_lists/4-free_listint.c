8c4c4529662ca4db5a29760728cedd0d578cdcf7
muhmadshaban <moshaban96.eng@gmail.com>
2023-09-26 00:11:17 +0300

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- ** free_listint :> frees a linked list
+ ** free_listint - frees a linked list
  ** @head: listint_t list to be empty
  **/
 void free_listint(listint_t *head)
