034a1f9ed94e270cced0ca70227c5611b310c1d7
muhmadshaban <moshaban96.eng@gmail.com>
2023-09-28 13:35:59 +0300

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,14 +5,14 @@
  ** @n: The pointer show  an unsigned long int.
  ** @index: index of the bit.
  **
- ** Return: 1 if it worked, 0 if it didn't.
+ ** Return: 1 if it worked, -1 if it didn't.
  **/
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
 	if (index > 63)
-	return (0);
+	return (-1);
 
 	m = 1 << index;
 	*n = (*n | m);
